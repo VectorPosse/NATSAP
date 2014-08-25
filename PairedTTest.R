eb59f11f6695b7cec007f73c570fb8744a29cb64
sofatfatso <ntk0213@westminstercollege.edu>
2014-08-25 09:27:50 -0600

    Changed PairedTTest program

--- PairedTTest.R
+++ PairedTTest.R
@@ -15,7 +15,20 @@ whichDiff <- function(x1, x2){
 
 
 ## Decide which variables to use for the Paired T-Test
-diff <- whichDiff("YOUTHOQAdmissionTotalScore", "YOUTHOQDischargeTotalScore")
+YAdd <- "YOUTHOQAdmissionTotalScore"
+YDis <- "YOUTHOQDischargeTotalScore"
+YPD6 <- "YOUTHOQPD6TotalScore"
+YPD12 <- "YOUTHOQPD12TotalScore"
+MAdd <- "MOTHEROQInitialYOQ20TotalScore"
+MDis <- "MOTHEROQDischarge20TotalScore"
+MPD6 <- "MOTHEROQPD6YOQ20TotalScore"
+MPD12 <- "MOTHEROQPD12YOQ20TotalScore"
+FAdd <- "FATHEROQInitiaLOQ20TotalScore"
+FDis <- "FATHEROQDischargeYOQ20TotalScore"
+FPD6 <- "FATHEROQPD6YOQ20TotalScore"
+FPD12 <- "FATHEROQPD12OQ20TotalScore"
+
+diff <- whichDiff(YAdd, YDis)
 diff <- diff[,1]
 diff <- diff[!is.na(diff)]
 N <- length(diff)
@@ -26,5 +39,4 @@ print(t.test(diff))
 fitData <- c("N","diff")
 fit <- stan(file = "YouthOQ_AD.stan", data = fitData)
 
-#Pull Request test
-pull_test <- "test"
+
